eb917ff6f063a42abdec69add1b443c493b73575
DerSkillBaumApfel <DerSkillBaumApfel@users.noreply.github.com>
2024-10-04 13:03:02 +0200

    :milky_way: dev: Minor fixes
    - Moved README to correct directory
    - Fixed typo in FACTORY_MAP
    - Trying to make automatic build/install work via github link

--- src/indexers/indexer-factory-map.ts
+++ src/indexers/indexer-factory-map.ts
@@ -6,6 +6,6 @@ import { Meilisearch } from "./meilisearch";
 // This map contains all available indexers and has to be extended for config name resolution to work.
 export const INDEXER_FACTORY_MAP = new Map<string, typeof IndexerInterface>([
 	["meilisearch", Meilisearch],
-	["elasticsearch", Elasticsearch]
+	["elasticsearch", Elasticsearch],
 	["algolia", Algolia]
 ])
